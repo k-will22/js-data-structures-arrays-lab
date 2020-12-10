cf3fda5feaa5eedbf1784c9bc81b5cf368c702b2
k-will22 <thestreetkw@hotmail.com>
2020-12-09 22:45:07 -0500

    commit tree 1dce84a6f2efcf195ccda23e61931fdaf74469d7

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver() {
+    drivers.push("Ralph")
+}
+
+function destructivelyPrependDriver() { 
+    drivers.unshift("Bob")
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift()
+}
+
+function appendDriver() {
+    let drive = [...drivers, "Broom"]
+    return drive 
+}
+
+function prependDriver() {
+    let drive = ["Arnold", ...drivers]
+    return drive 
+}
+
+function removeLastDriver() { 
+    let drive = drivers.slice(0, 2)
+    return drive
+}
+
+function removeFirstDriver() {
+    let drive = drivers.slice(1)
+    return drive
+}
